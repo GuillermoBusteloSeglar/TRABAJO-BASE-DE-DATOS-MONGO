d052838ad1b7ef13d92946bdc7171528d630f37f
GuillermoBusteloSeglar <gbustelo@iespuntadelverde.es>
2020-11-23 23:43:09 +0100

    first commit

---
+++ Readme.md
@@ -0,0 +1,13 @@
+-- 
+
+
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/GuillermoBusteloSeglar/TRABAJO-BASE-DE-DATOS-MONGO.git
+git push -u origin main
+
+
+
+--
---
+++ scr/InsertMany.js
@@ -0,0 +1,33 @@
+db.yumanyi.insertMany([
+    {
+        "_id": "Pel",
+        "item": "Peluches",
+        "qty": 20,
+        "Marca": "Ty"
+    },
+    {
+        "_id": "FirAn",
+        "item": "FigurasAnimadas", 
+        "qty": 15,
+        "Marca": "Banpresto"
+    },
+    {
+        "_id": "VidJue",
+        "item": "Videojuegos",
+        "qty": 45,
+        "Marca": ("EaSports, Activision, Sony, Microsoft")
+
+    },
+    {
+        "_id": "VidCon",
+        "item": "Videoconsolas",
+        "qty": 10,
+        "Marca": ("PlayStation, Microsoft, Nintendo")
+    },
+    {
+        "_id": "JueMes",
+        "item": "JuegosdeMesa",
+        "qty": 23,
+        "Marca": ("Hasbro")
+    }
+])
