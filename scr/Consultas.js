53264b2573b050be5fa0d6e87cd7e0edfd630824
GuillermoBusteloSeglar <gbustelo@iespuntadelverde.es>
2020-11-24 03:22:30 +0100

    Proyecto 2

--- scr/Consultas.js
+++ scr/Consultas.js
@@ -8,13 +8,7 @@ db.yumanyi.find({$or:[{ "Marca": "Banpresto"}, {"Precio":{$ne:400}}]})
 db.yumanyi.find( {"FechaEntrada" : {"$gt" : new Date("2020-12-9")}})
 //Con esta consulta se mostrarn todos los items cuyo precio no sea mayor de 15
 db.yumanyi.find( { "Precio": { $not: { $gt: 15 } } } )
-//
-db.yumanyi.find( { item: { $not: { $regex: /^J.*/, $options: 'i' } } } 
-//
-db.yumanyi.find()
-//
-db.yumanyi.find()
-//
-db.yumanyi.find()
-//
-db.yumanyi.find()
+//Con esta consulta se mostraran todas los items cuya marca no empiece por la letra T y que este no distinga entre mayusculas y minusculas
+db.yumanyi.find( { Marca: { $not: { $regex: /^T/, $options: 'i' } } })
+//Con este consulta se mostraran todos los items cuya marca no acabe en s y que este no distinga entre mayusculas y minusculas y o aquellos cuya fecha de entrada no es igual a la indicada
+db.yumanyi.find({$or:[{ "Marca": {$not: {$regex: /s$/, $options: 'i'}}}, {"FechaEntrada": {"$ne": new Date(2020-12-15)}}]})
