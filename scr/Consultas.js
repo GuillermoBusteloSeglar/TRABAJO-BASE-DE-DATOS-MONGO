35f432fb424822894e22bf2beff2cf915a11a159
GuillermoBusteloSeglar <gbustelo@iespuntadelverde.es>
2020-11-24 03:21:42 +0100

    Proyecto 2

--- scr/Consultas.js
+++ scr/Consultas.js
@@ -1,1 +1,20 @@
+//Con este consulta quiero que mongo me muestre las marcas las cuales empiecen por la letra b y que este no distinga entre mayusculas y minusculas
+db.yumanyi.find( { Marca: { $regex: /^B/, $options: 'i'} } )
+//Con esta consulta se mostraran todos los items que tengan un precio igual o superior a 400
+db.yumanyi.find({$and:[{ "qty": {lte:15}}, {"Marca":{$in:"Nintendo"}}]})
+//Con esta consulta se mostraran todos los items de marca Banpresto o a quellos que su precio sea distinto de 400
+db.yumanyi.find({$or:[{ "Marca": "Banpresto"}, {"Precio":{$ne:400}}]})
+//Con esta consulta se mostraran todos los items cuya fecha de entrada sea mayor o igual al 9/12/2020
+db.yumanyi.find( {"FechaEntrada" : {"$gt" : new Date("2020-12-9")}})
+//Con esta consulta se mostrarn todos los items cuyo precio no sea mayor de 15
+db.yumanyi.find( { "Precio": { $not: { $gt: 15 } } } )
+//
+db.yumanyi.find( { item: { $not: { $regex: /^J.*/, $options: 'i' } } } 
+//
+db.yumanyi.find()
+//
+db.yumanyi.find()
+//
+db.yumanyi.find()
+//
 db.yumanyi.find()
--- scr/InsertMany.js
+++ scr/InsertMany.js
@@ -1,33 +1,6 @@
 db.yumanyi.insertMany([
-    {
-        "_id": "Pel",
-        "item": "Peluches",
-        "qty": 20,
-        "Marca": "Ty"
-    },
-    {
-        "_id": "FirAn",
-        "item": "FigurasAnimadas", 
-        "qty": 15,
-        "Marca": "Banpresto"
-    },
-    {
-        "_id": "VidJue",
-        "item": "Videojuegos",
-        "qty": 45,
-        "Marca": ("EaSports, Activision, Sony, Microsoft")
-
-    },
-    {
-        "_id": "VidCon",
-        "item": "Videoconsolas",
-        "qty": 10,
-        "Marca": ("PlayStation, Microsoft, Nintendo")
-    },
-    {
-        "_id": "JueMes",
-        "item": "JuegosdeMesa",
-        "qty": 23,
-        "Marca": ("Hasbro")
-    }
-])
+    {"_id": "Pel", "item": "Peluches", "qty": 20, "Marca": "Ty", "Precio": 10, "FechaEntrada":new Date("2020-12-1")},
+    {"_id": "FirAn", "item": "FigurasAnimadas",  "qty": 15, "Marca": "Banpresto", "Precio": 24, "FechaEntrada":new Date("2020-12-29")},
+    {"_id": "VidJue", "item": "Videojuegos", "qty": 45, "Marca": ("EaSports, Activision, Sony, Microsoft"), "Precio": 70, "FechaEntrada":new Date("2020-12-15")},
+    {"_id": "VidCon", "item": "Videoconsolas", "qty": 10, "Marca": ("PlayStation, Microsoft, Nintendo"), "Precio": 400, "FechaEntrada": new Date("2020-12-9")},
+    {"_id": "JueMes", "item": "JuegosdeMesa", "qty": 23, "Marca": ("Hasbro"), "Precio": 30, "FechaEntrada": new Date("2020-12-15")}])
